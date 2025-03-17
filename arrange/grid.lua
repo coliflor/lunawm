31687fc167a5e27514b18e858240753d1a7b522c
Coliflor <javi.forano@gmail.com>
2025-03-17 19:33:30 +0100

    17-03-25

--- arrange/grid.lua
+++ arrange/grid.lua
@@ -21,20 +21,45 @@ local function arrange_grid(tag)
 	 local tile_height = (VRESH - gap_top - gap_bottom) / rows
 
 	 local index = 1
-	 for row = 1, rows do
-			for col = 1, cols do
+	 for col = 1, cols do
+			local col_windows = {}
+			for row = 1, rows do
 				 if index <= n then
-						local wnd = visible_windows[index]
+						table.insert(col_windows, visible_windows[index])
+						index = index + 1
+				 end
+			end
+
+			local col_n = #col_windows
+			local col_index = 1
+			local current_y = gap_top
+
+			for row = 1, rows do
+				 if col_index <= col_n then
+						local wnd = col_windows[col_index]
 						local pad_w = wnd.margin.l + wnd.margin.r
 						local pad_h = wnd.margin.t + wnd.margin.b
 
 						local x = (col - 1) * tile_width + wnd.margin.l + gap / 2 + gap_left
-						local y = (row - 1) * tile_height + wnd.margin.t + gap / 2 + gap_top
 
-						wnd:move(x, y)
-						wnd:resize(tile_width - pad_w - gap, tile_height - pad_h - gap)
+						if col_index == col_n then
+							 -- Last window in column: expand to fill remaining height if needed
+							 local remaining_height = VRESH - gap_top - current_y
+							 if remaining_height > tile_height then
+									wnd:move(x, current_y + wnd.margin.t + gap / 2)
+									wnd:resize(tile_width - pad_w - gap, remaining_height - pad_h - gap)
+							 else
+									wnd:move(x, current_y + wnd.margin.t + gap / 2)
+									wnd:resize(tile_width - pad_w - gap, tile_height - pad_h - gap)
+							 end
+						else
+							 -- Regular window: use row-based tile height
+							 wnd:move(x, current_y + wnd.margin.t + gap / 2)
+							 wnd:resize(tile_width - pad_w - gap, tile_height - pad_h - gap)
+							 current_y = current_y + tile_height
+						end
 
-						index = index + 1
+						col_index = col_index + 1
 				 end
 			end
 	 end
